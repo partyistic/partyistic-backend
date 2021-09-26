5a54f6a608e805cefad6e6a16e4bf943bca5b33b
YAHIAQOUS <yahiaqous@gmail.com>
2021-09-26 13:03:55 +0300

    Finishing Services Models

--- partyistic/serializers.py
+++ partyistic/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Inspiration, Services, Parties, Cars, Places
+from .models import Inspiration, Places, Planners, MusicBands ,PhotoSession, Fashion, Cars, Trip, Parties
 
 
 class InspirationSerializer(serializers.ModelSerializer):
@@ -7,26 +7,48 @@ class InspirationSerializer(serializers.ModelSerializer):
         model = Inspiration
         fields = "__all__"
 
-class CarsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cars
-        fields = '__all__'
+# class ServicesSerializer(serializers.ModelSerializer):
+#     cars = CarsSerializer()
+#     places = PlacesSerializer()
+#     class Meta:
+#         model = Services
+#         fields = '__all__'
+#         fields = ('service_type', 'cars', 'places')  
 
 class PlacesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Places
         fields = '__all__'
 
-
-class ServicesSerializer(serializers.ModelSerializer):
-    # cars = CarsSerializer(serializers.ModelSerializer)
-    # places = PlacesSerializer(serializers.ModelSerializer)
+class PlannersSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Services
+        model = Planners
         fields = '__all__'
-        # fields = ('service_type', 'cars', 'places')  
 
+class MusicBandsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MusicBands
+        fields = '__all__'
 
+class PhotoSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoSession
+        fields = '__all__'
+
+class FashionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fashion
+        fields = '__all__'
+
+class CarsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cars
+        fields = '__all__'
+
+class TripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = '__all__'
 
 
 class PartiesSerializer(serializers.ModelSerializer):
